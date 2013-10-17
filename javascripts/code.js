1731a70560e65191cfa12d2ff89d149936ae0de0
Sorrowhere <sorrowhere@gmail.com>
2013-10-17 23:27:45 +0800

    Init

---
+++ javascripts/code.js
@@ -0,0 +1,18 @@
+/*@Desciption:ff javascript*/
+/*@Author:Liu-yao*/
+/*@Date:2013/5/18*/
+
+//拒绝显示隐藏
+$(document).ready(function() {
+	$("#rej-bug").fancybox({
+		'titlePosition'		: 'inside',
+		'transitionIn'		: 'none',
+		'transitionOut'		: 'none'
+	});
+	$("#batch-add").fancybox({
+		'titlePosition'		: 'inside',
+		'transitionIn'		: 'none',
+		'transitionOut'		: 'none'
+	});
+
+});
